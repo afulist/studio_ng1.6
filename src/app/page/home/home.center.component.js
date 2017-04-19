6a7f8cabe1e7068a53a8447b2e38881e7b4c2c37
afulist <afulist@cht.com.tw>
2017-04-19 15:14:31 +0800

    add home-center and home-left

--- src/app/page/home/home.center.component.js
+++ src/app/page/home/home.center.component.js
@@ -6,6 +6,32 @@ angular
   });
 
 /** @ngInject */
-function HomeCenterController() {
-  // var vm = this;
+function HomeCenterController($log, mobileFeature) {
+  var vm = this;
+
+  // ==view data==
+  vm.ios = mobileFeature.ios;
+  var url = 'https://material.angularjs.org/1.0.6/img/list/60.jpeg';
+  vm.userImage = {'background-image': 'url(' + url + ')'};
+  vm.plansSort = [
+    {id: 1, name: '看最新'},
+    {id: 2, name: '看最多讚'},
+    {id: 3, name: '看最多收藏'},
+    {id: 4, name: '看最多天'},
+    {id: 5, name: '看最少天'}
+    // { id: 1, name: '最多留言最前'}
+  ];
+  vm.selectedSort = 3;
+  // vm.loc = '最新';
+  vm.loc = '台灣';
+
+  // ==init func==
+  vm.$onInit = function () {
+    // TODO-selectLoc:
+    // // TODO-load: on event to load plans
+    // var loadPlans = $rootScope.$on('selectLoc', function(event, args) {
+    //   vm.loc = args.loc.name;
+    // });
+    // $scope.$on('$destroy', loadPlans);
+  };
 }
--- src/app/page/home/home.left.component.js
+++ src/app/page/home/home.left.component.js
@@ -6,6 +6,44 @@ angular
   });
 
 /** @ngInject */
-function HomeLeftController() {
-  // var vm = this;
+function HomeLeftController($log, $mdSidenav) {
+  var vm = this;
+
+  // ==view data==
+  vm.locations = [
+    {locId: 0, name: '台灣', img: 'img/100-0.jpeg', newMessage: true},
+    {locId: 1, name: '日本', img: 'img/100-0.jpeg', newMessage: true},
+    {locId: 2, name: '韓國', img: 'img/100-1.jpeg', newMessage: false},
+    {locId: 3, name: '泰國', img: 'img/100-2.jpeg', newMessage: false}
+  ];
+
+  // ==view func==
+  vm.selectLoc = selectLoc;
+  vm.test = test;
+
+  // ==init func==
+  vm.$onInit = function () {
+
+  };
+
+  // close Sidenav without click locking
+  function selectLoc(loc, menuId) {
+    if (loc === '最新') {
+      // change source
+      loc = {locId: 0, name: '最新', img: 'img/100-0.jpeg', newMessage: true};
+    }
+    // scroll to top
+    document.getElementById('top').scrollIntoView(true); // eslint-disable-line
+    // close nav
+    if (menuId) {
+      $mdSidenav(menuId).close();
+    }
+    // TODO-selectLoc: broadcast to load plans
+    // $rootScope.$broadcast('selectLoc', {loc: loc});
+  }
+
+  // test
+  function test() {
+    $log.debug('test');
+  }
 }
